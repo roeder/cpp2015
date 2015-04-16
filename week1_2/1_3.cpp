53045ea59f40d0e728cd61df44529f4184ee025a
roeder <roeder.t17@gmail.com>
2015-04-16 18:20:20 +0200

    1.2 done

---
+++ week1_2/1_3.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+
+int main(int argc, char* argv[]) {
+
+    double vector1[3] = {2, 4, 7};
+    double vector2[3] = {3, 1, 5};
+
+    std::cout << "First vector " << vector1 << "\n";
+    std::cout << "Second vector " << vector2 << "\n";
+
+//    std::cout << "Enter second number\n";
+//    std::cin >> in2;
+//
+//    out1 = in1 * in2;
+//
+//    std::cout << "Product is " << out1 << "\n";
+
+    return 0;
+}
+
