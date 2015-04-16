48829bc34f6c36af89e976234a380d0f02a7c587
roeder <roeder.t17@gmail.com>
2015-04-16 20:52:54 +0200

    1.4 initialised

---
+++ week1_2/1_4.cpp
@@ -0,0 +1,5 @@
+/*Write code that declares four 2 × 2 matrices of double precision floating point numbers,A,B,C,D,
+ *and assigns values to the entries ofAandB. LetC=A+B, and D=AB. Extend your code so that it
+ * calculates the entries of C and D, and then prints the entries of these matrices to screen.*/
+
+
