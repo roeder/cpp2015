0140925c1b1c4dea97b7f5af4a1b294192928422
roeder <roeder.t17@gmail.com>
2015-04-20 15:06:45 +0200

    intitialised chapter 2

---
+++ week1_2/2_1.cpp
@@ -0,0 +1,20 @@
+/*
+Below is an example fragment of code that uses several features introduced in this chapter. The variables x, y and z
+ are all double precision floating point variables.*/
+
+//double x, y, z;
+//if ((x > y) || (x < 5.0)) {
+//    z = 4.0; }
+//else
+//{
+//    z = 2.0;
+//}
+
+/*
+1. Explain, in words, what the fragment of code does.
+2. What value would the fragment of code assign to the variable z when the vari-
+ables x and y take the following values: (a) x=10.0, and y=-1.0;
+(b) x=10.0, and y=20.0; and
+(c) x=0.0, and y=20.0.
+3. Modifythecodeabovesothattheconditionx>yisreplacedbyx≥y
+ */
