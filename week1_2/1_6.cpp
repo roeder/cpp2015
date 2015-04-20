7285fbe96c6cdd55db2e5542eaa45c86d85e32cd
roeder <roeder.t17@gmail.com>
2015-04-20 13:59:36 +0200

    solved 1.6

--- week1_2/1_6.cpp
+++ week1_2/1_6.cpp
@@ -1,7 +1,14 @@
-/*
-I want to record the number of cars that drive past my house each day for five consecutive days, and calculate the
-average of these numbers. Create an integer array to store these five numbers, and then write code to calculate the
- average of these numbers. Execute your code using the sample data 34, 58, 57, 32, 43. Verify that you get the
- correct answer of 44.8.
- Hint: read the material in Sect. 1.4.4 on converting integers to double precision floating point numbers.
-*/
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    int cars[] = {34, 58, 57, 32, 43};
+    double out1;
+
+    out1 = double(cars[0] + cars[1] + cars[2] + cars[3] + cars[4]) / 5.0;
+
+    std::cout << "Average number of cars is " << out1 << "\n";
+
+    return 0;
+}
+
