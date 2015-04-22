8cdcb90b3708278b597757ff27fe01ee47ba837e
roeder <roeder.t17@gmail.com>
2015-04-22 15:00:33 +0200

    solved 2.5

--- week1_2/2_5.cpp
+++ week1_2/2_5.cpp
@@ -3,4 +3,34 @@
  * 1. Write code to calculate the inverse of the matrix given by A = {{4, 10},{1, 1}}
  * 2. Check that the inverse calculated is correct by printing out the entries of the inverse, and comparing
  * with direct calculation.
- * 3. Modify your code to include an assert statement that checks that the determinant of the matrix is nonzero.
+ * 3. Modify your code to include an assert statement that checks that the determinant of the matrix is nonzero.*/
+
+#include <iostream>
+#include "cassert"
+#include "cmath"
+
+int main(int argc, char* argv[]) {
+
+    int A[2][2] = {{4, 10}, {1, 1}};
+    int det_A;
+
+    det_A = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+    assert(det_A != 0);
+
+    double inv_A[2][2];
+
+    std::cout << "Inverse matrix is\n";
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            if (i == j) {
+                inv_A[i][j] = A[std::abs(i -1)][std::abs(j -1)] / double(det_A);
+            } else {
+                inv_A[i][j] = -A[i][j] / double(det_A);
+            }
+            std::cout.flush() << inv_A[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
---
+++ week1_2/playground.R
@@ -0,0 +1,21 @@
+for (i in paste0('5_', 1:10, '.cpp')) {
+  cmd <- paste('touch', i)
+  system(cmd)
+}
+  
+# 2.4
+u <- c(1, 2, 3)
+v <- c(6, 5, 4)
+A <- matrix(c(1, 5, 0, 7, 1, 2, 0, 0, 1), nrow = 3, byrow = T)
+B <- matrix(c(-2, 0, 1, 1, 0, 0, 4, 1, 0), nrow = 3, byrow = T)
+w <- u - 3 * v
+
+x <- u - v
+y <- A %*% u
+z <- A %*% u -v
+C <- 4 * A - 3 * B
+D <- A %*% B
+
+# 2.5
+A <- matrix(c(4, 10, 1, 1), nrow = 2, byrow = T)
+inv_A <- solve(A)
