fee83367d0660729d4aa70ecd074525dd971ff5d
roeder <roeder.t17@gmail.com>
2015-04-22 13:38:01 +0200

    solved 2.4

--- week1_2/2_4.cpp
+++ week1_2/2_4.cpp
@@ -1,6 +1,13 @@
 /* This exercise uses the following vectors and matrices: u, v, A, B.
  * Furthermore, the vector w satisfies w = u − 3v. These vectors and matrices are stored in arrays using the following
- * program. This program includes code to calculate the vector w. */
+ * program. This program includes code to calculate the vector w.
+ * We now define vectors x, y, and z, and matrices C and D, such that
+ *
+ * x = u − v,
+ * y = Au,
+ * z = Au − v,
+ * C = 4A − 3B,
+ * D = AB */
 
 #include <iostream>
 int main(int argc, char* argv[])
@@ -17,20 +24,59 @@ int main(int argc, char* argv[])
     double w[3];
     for (int i=0; i<3; i++)
     {
-        w[i] = u[i] - 3.0*v[i];
+        w[i] = u[i] - 3.0 * v[i];
+    }
+
+    std::cout << "Vector x = u - v is\n";
+    double x[3];
+    for (int j = 0; j < 3; j++) {
+        x[j] = u[j] - v[j];
+        std::cout.flush() << x[j] << '\n';
+    }
+
+    std::cout << "Vector y = Au is\n";
+    double y[3];
+    for (int i = 0; i < 3; ++i) {
+        y[i] = 0;
+        for (int j = 0; j < 3; ++j) {
+            y[i] += A[i][j] * u[j];
+        }
+        std::cout.flush() << y[i] << '\n';
+    }
+
+    std::cout << "Vector z = Au - v is\n";
+    double z[3];
+    for (int i = 0; i < 3; ++i) {
+        z[i] = 0;
+        for (int j = 0; j < 3; ++j) {
+            z[i] += A[i][j] * u[j];
+        }
+        z[i] -= v[i];
+        std::cout.flush() << z[i] << '\n';
+    }
+
+    std::cout << "Matrix C = 4A - 3B is\n";
+    double C[3][3];
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            C[i][j] = 4 * A[i][j] - 3 * B[i][j];
+            std::cout.flush() << C[i][j] << " ";
+        }
+        std::cout.flush() << '\n';
+    }
+
+    std::cout << "Matrix D = AB is\n";
+    double D[3][3];
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            D[i][j] = 0;
+            for (int k = 0; k < 3; ++k) {
+                D[i][j] += A[i][k] * B[k][j];
+            }
+            std::cout.flush() << D[i][j] << " ";
+        }
+        std::cout.flush() << '\n';
     }
 
     return 0;
 }
-
-/* We now define vectors x, y, and z, and matrices C and D, such that
- *
- * x = u − v,
- * y = Au,
- * z = Au − v,
- * C = 4A − 3B,
- * D = AB.
- *
- * Develop the program above to calculate the vectors x, y, and z and the matrices C and D, using loops where possible.
- * Hint: make sure you define arrays of an appropriate size for these variables. Check your answer by printing out
- * the results, and comparing with direct calculation.
