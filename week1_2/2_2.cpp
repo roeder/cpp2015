7307502c85500641456fee0f85bd5ab06d6a10c7
roeder <roeder.t17@gmail.com>
2015-04-22 12:35:54 +0200

    solved 2.2

--- week1_2/2_2.cpp
+++ week1_2/2_2.cpp
@@ -1,15 +1,37 @@
 /*Below is some example code. The exercises below all require modification of this code. In all cases use a suitable
  * check to ensure your code is correct. */
 
+/* 1. Set the variable x to the value 5 if either p is greater than or equal to q, or the variable j is not equal to 10.
+2. Set the variable x to the value 5 if both y is greater than or equal to q, and the variable j is equal to 20. If this
+ compound condition is not met, set x to take the same value as p.
+ 3. Setthevariablexaccordingtothefollowingrule. x = 0 if p > q; x = 1 if p <= q and j = 10; x = 2 otherwise. */
+
 #include <iostream>
 int main(int argc, char* argv[]) {
     double p, q, x, y;
     int j;
 
+    // Question 1
+    if (p >= q || j != 10) {
+        x = 5;
+    }
+
+    // Question 2
+    // Shorthand:
+    // x = y >= q && j == 20 ? 5 : p;
+    if (y >= q && j == 20) {
+        x = 5;
+    } else {
+        x = p;
+    }
+
+    // Question 3
+    x = 2;
+    if (p > q) {
+        x = 0;
+    }
+    if (p <= q && j == 10) {
+        x = 1;
+    }
     return 0;
 }
-
-/* 1. Set the variable x to the value 5 if either p is greater than or equal to q, or the variable j is not equal to 10.
-2. Set the variable x to the value 5 if both y is greater than or equal to q, and the variable j is equal to 20. If this
- compound condition is not met, set x to take the same value as p.
- 3. Setthevariablexaccordingtothefollowingrule. x = 0 if p > q; x = 1 if p <= q and j = 10; x = 2 otherwise. */
