cd9b6b19ba07ae820145a090c33619a63d2be3b6
roeder <roeder.t17@gmail.com>
2015-05-04 13:40:29 +0200

    2_6 update

--- week1_2/2_6.cpp
+++ week1_2/2_6.cpp
@@ -1,17 +1,19 @@
 #include "iostream"
 #include "numeric"
 #include "cmath"
+#include "2_6.h"
 
-int main(int argc, char* argv[]) {
+//int main(int argc, char* argv[]) {
+//
+//    std::cout.flush() << newton_Raphson(0, 0.1);
+//
+//    return 0;
+//}
 
-    double epsilon;
-    epsilon = 0.1;
-
+double newton_Raphson(double initialGuess, double epsilon)
+{
     double x_prev;
-    x_prev = 10;
-
-    double x_next;
-    x_next = 0;
+    x_prev = initialGuess + 2 * epsilon;
 
     const double e = 2.71828;
     double fx, der_fx;
@@ -19,14 +21,13 @@ int main(int argc, char* argv[]) {
     int i;
     i = 1;
 
-    while (std::abs(x_next - x_prev) >= epsilon) {
-        x_prev = x_next;
+    while (std::abs(initialGuess - x_prev) >= epsilon) {
+        x_prev = initialGuess;
         fx = pow(e, x_prev) + pow(x_prev, 3) - 5;
         der_fx = 3 * pow(x_prev, 2) + pow(e, x_prev);
-        x_next = x_prev - (fx / der_fx);
-        std::cout.flush() << "Iteration " << i << ": " << x_next << '\n';
+        initialGuess = x_prev - (fx / der_fx);
         i++;
     }
 
-    return 0;
+    return initialGuess;
 }
